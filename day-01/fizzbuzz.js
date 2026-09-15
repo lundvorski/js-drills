b35639bd609de8518e9644d1fb21c74f1b0c4783
Lun Dvorski <dvorski.lun@gmail.com>
2026-09-15 16:42:53 +0200

    fix: add export

--- day-01/fizzbuzz.js
+++ day-01/fizzbuzz.js
@@ -1,9 +1,13 @@
+export function fizzBuzz(num) {
+    let printString = '';
+    for(let i = 1; i <= N; i++) {
+        if(i % 3 == 0) printString += 'Fizz';
+        if(i % 5 == 0) printString += 'Buzz';
+        console.log(printString === '' ? i : printString);
+        printString = '';
+    }
+}
+
 const N = 100;
 
-let printString = '';
-for(let i = 1; i <= N; i++) {
-    if(i % 3 == 0) printString += 'Fizz';
-    if(i % 5 == 0) printString += 'Buzz';
-    console.log(printString == '' ? i : printString);
-    printString = '';
-}
+fizzBuzz(N);
