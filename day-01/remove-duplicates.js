a4564d741353a36842dc0af3bb78d8e73e9d7bb1
Lun Dvorski <dvorski.lun@gmail.com>
2026-09-15 16:23:39 +0200

    fix: add export, use set

--- day-01/remove-duplicates.js
+++ day-01/remove-duplicates.js
@@ -1,8 +1,11 @@
-const array = ['erd', 'pas', 'las', 'pas', 'mas', 'erd'];
-let removedDuplicatesArray = [];
+export function removeDuplicates(array) {
+    return [...new Set(array)];
 
-array.forEach(x => {
-    if(!removedDuplicatesArray.includes(x)) removedDuplicatesArray.push(x);
-})
+}
 
-console.log(removedDuplicatesArray);
+import assert from 'node:assert';
+assert.strictEqual(removeDuplicates([-5, -2, -10, -10, 'arr', 'arr']).length, 4);
+assert.strictEqual(removeDuplicates([]).length, 0);
+assert.strictEqual(removeDuplicates([7]).length, 1);
+assert.strictEqual(removeDuplicates([5, 5, 3]).length, 2);
+console.log('ok');
